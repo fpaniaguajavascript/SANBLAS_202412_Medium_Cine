ffbd3c31fd9f6bf5748ac2091624ba5b476bdd8d
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-24 10:40:31 +0100

    Cambio de iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { personOutline, personSharp, pawOutline, pawSharp, skullOutline, skullSharp, videocamOutline, videocamSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +13,14 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'All', url: '/folder/All', icon: 'mail' },
-    { title: 'Horror', url: '/folder/Horror', icon: 'paper-plane' },
+    { title: 'All', url: '/folder/All', icon: 'videocam' },
+    { title: 'Horror', url: '/folder/Horror', icon: 'skull' },
     { title: 'Drama', url: '/folder/Drama', icon: 'heart' },
-    { title: 'Misterio', url: '/folder/Mystery', icon: 'archive' },
-    { title: 'About', url: '/about', icon: 'trash' },
+    { title: 'Misterio', url: '/folder/Mystery', icon: 'paw' },
+    { title: 'About', url: '/about', icon: 'person' },
   ];
   //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({ personOutline, personSharp, pawOutline, pawSharp, skullOutline, skullSharp, videocamOutline, videocamSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
 }
